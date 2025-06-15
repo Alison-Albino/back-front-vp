1cb7124061de1e3003f2f600591b86b1fcb62c37
Alison-Albino <alisonalbino13@gmail.com>
2025-06-15 20:02:19 -0300

    Configurar Strapi para armazenamento local e remover Cloudinary

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,17 +1,3 @@
 module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-        upload_preset: 'strapi_upload',
-        use_filename: true,
-        unique_filename: true,
-        overwrite: true,
-        resource_type: 'auto',
-      },
-    },
-  },
+
 }); 
